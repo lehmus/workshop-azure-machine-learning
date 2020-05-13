b61084d3a8197196372f8db482d1db79d0a41f0e
lehmus <16114036+lehmus@users.noreply.github.com>
2020-05-13 13:33:34 +0300

    Lab 2 & 3 answers

--- code/explore/train_15models_submit.py
+++ code/explore/train_15models_submit.py
@@ -1,19 +1,34 @@
-"""
+'''
 Training submitter
 
 Facilitates (remote) training execution through the Azure ML service.
-"""
+'''
 
 # impot required packages
+import os
+from azureml.core import Workspace, Experiment
+from azureml.train.estimator import Estimator
+from azureml.core.authentication import AzureCliAuthentication
 
 # load Azure ML workspace
+workspace = Workspace.from_config(auth=AzureCliAuthentication())
 
 # Define Run Configuration
+est = Estimator(
+    entry_script='train_15models.py',
+    source_directory=os.path.dirname(os.path.realpath(__file__)),
+    compute_target='local',
+    user_managed=True,
+    use_docker=False
+)
 
 # Define the ML experiment
+experiment = Experiment(workspace, 'newsgroups_train_15models')
 
-# Submit experiment run, if compute is idle, this may take some time')
+# Submit experiment run, if compute is idle, this may take some time
+run = experiment.submit(est)
 
 # wait for run completion of the run, while showing the logs
+run.wait_for_completion(show_output=True)
 
 # Select best model from run
